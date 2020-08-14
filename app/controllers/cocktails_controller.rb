442ab3ed97429096fa1b2f750aa0501038cfc4ec
Andrea Kreideweiss <akreideweiss@gmail.com>
2020-08-14 16:38:53 +0200

    added image upload and enhanced scss

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -23,6 +23,6 @@ class CocktailsController < ApplicationController
   private
 
   def strong_params
-    params.require(:cocktail).permit(:name)
+    params.require(:cocktail).permit(:name, :photo)
   end
 end
